9b6c21ea2816f55bbfbfae2e886e96cfd846814a
jerryWTMH <jerry1202.wtmh@gmail.com>
2022-11-30 16:39:38 -0500

    modify the input parameters for the functions

--- simulator_sequential.h
+++ simulator_sequential.h
@@ -8,9 +8,9 @@
 int * getModularPos(int n, int i, int j, int ** elevationMatrix);
 int ** readElevationFile(const char * elevationFilename, int N);
 int *** getDirection(int ** elevationMatrix);
-void drop(double** ground);
-void absorb(double ** ground, double amount, double ** absorption);
-bool flow(double ** result, int *** direction);
+void drop(double** ground, size_t n);
+void absorb(double ** ground, double amount, double ** absorption, size_t n);
+bool flow(double ** result, int *** direction, size_t n);
 void freeFlowMatrix(double ** flowMatrix, size_t n);
 
 
